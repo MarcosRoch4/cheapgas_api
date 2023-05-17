8c7c7a2943cef6f9407f403163bf932f55a2060a
Marcos Rocha <44954061+MarcosRoch4@users.noreply.github.com>
2023-05-17 17:46:30 -0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -13,7 +13,8 @@ var (
 )
 
 func ConectaDB() {
-	dbConect := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	//dbConect := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	dbConect := "host=mouse.db.elephantsql.com user=lgngbpdo password=5NqqDGWFkazYmraBjtPhJAeUoJzFJ5Kt dbname=lgngbpdo port=5432 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(dbConect), &gorm.Config{})
 	if err != nil {
 		log.Panic("Erro ao conectar com o Banco de dados")
